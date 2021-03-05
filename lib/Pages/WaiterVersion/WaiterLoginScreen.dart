eaad40d73a550f4bfe1ea3f47ab2c0c75a2cc4aa
davidkt99 <49345822+davidkt99@users.noreply.github.com>
2021-03-05 00:21:12 -0600

    Added Waiter/Manager Login Screen

--- lib/Pages/WaiterVersion/WaiterLoginScreen.dart
+++ lib/Pages/WaiterVersion/WaiterLoginScreen.dart
@@ -1,11 +1,67 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WaiterLoginScreen extends StatelessWidget {
+  String password = "";
+  String username = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Waiter Login Screen"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child:  Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              //color: Colors.teal,
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 100),
+              child: Center(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'User Name',
+                    hintText: 'Enter Your Name',
+                  ),
+                  onChanged: (text){
+                    username = text;
+                  },
+                ),
+              ),
+            ),
+            Container(
+              //color: Colors.teal,
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 100),
+              child: Center(
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter Password',
+                  ),
+                  onChanged: (text){
+                    password = text;
+                  },
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+                minimumSize: Size(150,50),
+              ),
+                onPressed: (){
+                print(["username = ",username]);
+                print(["password = ",password]);
+            }, child: Text("Login"))
+          ],
+        ),
       ),
     );
   }
