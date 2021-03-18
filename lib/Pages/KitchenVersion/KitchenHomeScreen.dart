0fbff5c7adee30fd334808c51ced682b1ace8f88
Andy <ach0275@unt.edu>
2021-03-18 16:37:47 -0500

    changing page to implement database

--- lib/Pages/KitchenVersion/KitchenHomeScreen.dart
+++ lib/Pages/KitchenVersion/KitchenHomeScreen.dart
@@ -1,4 +1,5 @@
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:virtual_restaurant/Data/globals.dart' as globals;
 import 'package:virtual_restaurant/classes/menuItem.dart';
@@ -8,13 +9,23 @@ import 'package:virtual_restaurant/Database/database.dart';
 
 
 class KitchenHomeScreen extends StatefulWidget {
+  // These two lines needed for firebase
+    KitchenHomeScreen({this.app});
+    final FirebaseApp app;
+  // -----------------------------------
   @override
   _KitchenHomeScreenState createState() => _KitchenHomeScreenState();
 }
 
 class _KitchenHomeScreenState extends State<KitchenHomeScreen> {
-  List orders = List();
 
+  List orders = List();
+  List identifier = List();
+  Color changeIconColor = Colors.grey;
+  String input = "";
+
+  // using this to grab database information
+  final referenceDatabase = FirebaseDatabase.instance;
 
   @override
   void initState() {
@@ -23,11 +34,41 @@ class _KitchenHomeScreenState extends State<KitchenHomeScreen> {
     orders.add("item2");
   }
 
+  // Varibles
+  final OrderNumber = 'OrderNumber';
   @override
   Widget build(BuildContext context) {
+
+    final ref = referenceDatabase.reference();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Orders Todo"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context){
+              return AlertDialog(
+                title: Text("Add new order"),
+                content: TextField(
+                  onChanged: (String value){
+                    input = value;
+                  },
+                ),
+                actions: <Widget>[
+                FlatButton(onPressed: (){
+                  setState(() {
+                    orders.add(input);
+                  });
+                  Navigator.of(context).pop(); // Closes the Alert Dialog Box
+                } , child: Text("Add"))
+                ],
+              );
+            }
+          );
+        },
       ),
       body: ListView.builder(
           itemCount: orders.length,
@@ -36,24 +77,44 @@ class _KitchenHomeScreenState extends State<KitchenHomeScreen> {
                 //key: orders[index],
                 key: UniqueKey(),
                 child: Card(
+                    color: Colors.white54,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(16)
                     ),
+
                     child: ListTile(
+                      selected: true,
                       title: Text(orders[index]),
+                      tileColor:  Colors.white54,
+                     // selectedTileColor: Colors.green,
+                      //focusColor: Colors.blue,
+
                       trailing: IconButton(
+                         // highlightColor: Colors.orange,
+
                           icon: Icon(
-                              Icons.delete, 
-                              color: Colors.red
-                          ), 
+                              Icons.timer,
+                              color: changeIconColor,
+                              // Icons.delete,
+                              // color: Colors.red,
+                          ),
                           onPressed: (){
                             setState(() {
-                              orders.removeAt(index);
+                              changeIconColor = Colors.orange;
+
                             });
-                          }),
+                          },
+                          // onPressed: (){
+                          //   setState(() {
+                          //     orders.removeAt(index);
+                          //   });
+                          //}
+                      ),
                     ),
-            ));
-          }),
+                ),
+
+            );
+      }),
     );
   }
 }
