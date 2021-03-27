782f9505ab15984edaedde54119587df07fbac72
davidkt99 <49345822+davidkt99@users.noreply.github.com>
2021-03-26 23:55:49 -0500

    ChangeMealOfDayPage mostly done, needs database functionality and ability to change selected meal

--- lib/Pages/ManagerVersion/ChangeMealOfDayPage.dart
+++ lib/Pages/ManagerVersion/ChangeMealOfDayPage.dart
@@ -1,68 +1,238 @@
 import 'package:virtual_restaurant/Data/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:virtual_restaurant/Data/globals.dart';
+import 'package:virtual_restaurant/classes/menuItem.dart';
 
 /*
 This is where the manager can edit the menu that the customer can see
  */
+
+List<MenuItem> mChoiceList = [...entrees, ...appetizers, ...sides, ...desserts];
+int mealOfday_place = 0;
 
 class ChangeMealOfDayPage extends StatefulWidget {
   @override
   _ChangeMealOfDayPageState createState() => _ChangeMealOfDayPageState();
 }
 
-
 class _ChangeMealOfDayPageState extends State<ChangeMealOfDayPage> {
-
-
-  Widget item_Card(){
-    return Container(
-      height: 40,
-      width: 40,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.black45,
-        ),
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Center(
-        child: Text('hello'),
-      ),
-    );
-  }
+  List<Widget> grid_items = [];
+  //TODO: Change base to currently chosen meal of day
 
 
   @override
   Widget build(BuildContext context) {
+    for (int i = 0; i < mChoiceList.length; i++) {
+      grid_items.add(item_Card(
+        place: i,
+        onChanged: (place){
+          setState(() {
+            mealOfday_place = place;
+          });
+          print(["Meal Of Day: ",mealOfday_place]);
+        }
+      ));
+    }
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(70.0),
         child: AppBar(
-            title: Text(
-              "Change Meal of Day",
-              style: kAppBarTextStyle,
-            ),
+          title: Text(
+            "Change Meal of Day",
+            style: kAppBarTextStyle,
+          ),
         ),
       ),
       body: Column(
         children: <Widget>[
           Expanded(
             flex: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                color: Colors.black,
+              )),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(15),
+                    child: Text(
+                      'Meal of the Day: ',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Expanded(
+                    child: MealOfDay(
+                      name: mChoiceList[mealOfday_place].getItemName,
+                      allergens: mChoiceList[mealOfday_place].getAllergens,
+                      calories: mChoiceList[mealOfday_place].getCalories,
+                      description: mChoiceList[mealOfday_place].getDescription,
+                      price: mChoiceList[mealOfday_place].getPrice,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                ),
+              ),
+              child: GridView.count(
+                primary: true,
+                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 40),
+                crossAxisSpacing: 40,
+                mainAxisSpacing: 40,
+                crossAxisCount: 3,
+                childAspectRatio: 5 / 3,
+                children: grid_items,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class item_Card extends StatelessWidget {
+  int place;
+  Function onChanged;
+
+  item_Card({this.place, this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.green[800],
+      ),
+      onPressed: () {
+        print([mChoiceList.length, place]);
+        onChanged;
+      },
+      child: Center(
+        child: Text(mChoiceList[place].getItemName),
+      ),
+    );
+  }
+}
+
+class MealOfDay extends StatelessWidget {
+  final Image image;
+  final String name;
+  final String price;
+  final int calories;
+  final String description;
+  final List<String> allergens;
+
+  MealOfDay({
+    this.image,
+    @required this.name,
+    @required this.allergens,
+    @required this.calories,
+    @required this.description,
+    @required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 0.2),
+        ),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Container(
+              margin: EdgeInsets.only(right: 10.0),
+              width: 250,
+              color: Colors.grey,
+              child: Center(
+                child: Text("Image"), //TODO: Replace text with image
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Container(
-                  margin: EdgeInsets.all(15),
-                  child: Text('Meal of the Day: ', style: TextStyle(fontSize: 20),),
+                Text(
+                  name,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
                 ),
-                Center(
-                  child: Text('TADA'),
+                Text(
+                  price,
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+                Text(
+                  "$description",
+                  style: TextStyle(fontSize: 17.0),
+                ),
+                Text(
+                  "$calories calories",
+                  style: TextStyle(fontSize: 17.0),
+                ),
+                Text(
+                  "Allergens: $allergens",
+                  style: TextStyle(fontSize: 17.0),
                 ),
               ],
             ),
           ),
           Expanded(
-            flex: 2,
-            child: GridView.count(
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.only(right: 15.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      duration: Duration(seconds: 1),
+                      content: Text("$name is now the Meal Of The Day!"),
+                    ));
+                    print("Add to Meal of Day pressed");
+
+                    //TODO: Add to order functionality: change so this supports adding to Meal Of Day Database location
+
+                  },
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(kLightGreen),
+                    elevation: MaterialStateProperty.all(4.0),
+                    overlayColor:
+                        MaterialStateProperty.all<Color>(Colors.green[300]),
+                  ),
+                  child: Container(
+                    height: 40,
+                    child: Center(
+                      child: Text(
+                        "Change",
+                        style: TextStyle(
+                          fontSize: 20,
+                          //TODO: Have text adapt to container size when changing screen orientation
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
         ],
