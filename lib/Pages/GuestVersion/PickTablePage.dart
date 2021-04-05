71bcc0ff89ead65166dab5621ba613e4e381c4ab
Mary Plana <MaryCharmaineLoiPlana@my.unt.edu>
2021-04-05 16:46:47 -0500

    editted ui for these pages

--- lib/Pages/GuestVersion/PickTablePage.dart
+++ lib/Pages/GuestVersion/PickTablePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:virtual_restaurant/Data/constants.dart';
 import 'package:virtual_restaurant/Data/globals.dart';
 
 class PickTablePage extends StatefulWidget {
@@ -8,13 +9,10 @@ class PickTablePage extends StatefulWidget {
 
 class _PickTablePageState extends State<PickTablePage> {
   @override
-
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Current Table: ${tableID}'),
-
       ),
       body: Center(
         child: GridView.builder(
@@ -22,24 +20,30 @@ class _PickTablePageState extends State<PickTablePage> {
             padding: EdgeInsets.all(20),
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 5,
-                childAspectRatio: 3 / 2,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20),
+                childAspectRatio: 1.70,
+                crossAxisSpacing: 30,
+                mainAxisSpacing: 30),
             itemCount: 20,
             itemBuilder: (BuildContext ctx, index) {
-              return GestureDetector(
-                onTap: (){
+              return InkWell(
+                onTap: () {
                   setState(() {
-                    tableID = "T${index+1}";
+                    tableID = "T${index + 1}";
                   });
                   print(tableID);
                 },
                 child: Container(
                   alignment: Alignment.center,
-                  child: Text("T${index+1}"),
+                  child: Text(
+                    "T${index + 1}",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
                   decoration: BoxDecoration(
-                      color: Colors.green[800],
-                      borderRadius: BorderRadius.circular(15)),
+                    color: kGreen,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
                 ),
               );
             }),
